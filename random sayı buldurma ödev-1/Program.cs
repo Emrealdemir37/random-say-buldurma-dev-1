a96c8bb6333ec8f1b85f0dc8743d8f654a105fa1
Misafir <Misafir@DESKTOP-DQET3G5>
2023-05-28 13:08:01 +0300

    Add project files.

---
+++ random sayı buldurma ödev-1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace random_sayı_buldurma_ödev_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int sayı = rnd.Next(1, 100);
+            int hak = 5;
+            Console.WriteLine("Random Çıkan Sayı:" + sayı);
+
+            while (0 < hak)
+            {
+                Console.WriteLine("Random Çıkan Sayıyı Tahmin Ediniz:");
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+                if (tahmin == sayı)
+                {
+                    Console.WriteLine("Tebrikler Doğru Tahminde Bulundunuz");
+                    Thread.Sleep(3000);
+                    Environment.Exit(0);
+                }
+                else if (hak == 1)
+                {
+                    Console.WriteLine("Yanlış Tahminde Bulundunuz Hakkınız Bitti ");
+                    break;
+
+                }
+
+                else if (tahmin < sayı)
+                {
+                    Console.WriteLine("Sayıdan Az Tahminde Bulundunuz Tekrar Deneyiniz");
+                    hak--;
+                    Console.WriteLine("Kalan Deneme Hakkınız:" + hak);
+
+                }
+                else
+                {
+                    Console.WriteLine("Sayıdan Yüksek Tahminde Bulundunuz Tekrar Deneyiniz");
+                    hak--;
+                    Console.WriteLine("Kalan Deneme Hakkınız:" + hak);
+                }
+
+
+
+            }
+            Console.WriteLine("Sizi 3 Evetle Uğurluyorum");
+            Thread.Sleep(3000);
+            Environment.Exit(0);
+        }
+    }
+}
